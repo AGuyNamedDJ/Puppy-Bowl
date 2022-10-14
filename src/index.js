979bdb5cefb8be89fce0bcd32ec02e7eee923ffd
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-14 15:32:27 -0500

    updated catch

--- src/index.js
+++ src/index.js
@@ -19,13 +19,20 @@ const Contestants = () => {
             try {
                 // Fetch method that will get some data from the URL;
                 const response = await fetch ("https://fsa-puppy-bowl.herokuapp.com");
+
                 // Convert promise response into JSON;
+                const data = await response.json();
+                console.log("Here is the translated data from the promise response: ", data)
+                console.log("Here is the REAL data from the above translated data", data.results)
 
                 // Sava data from response promise & update state;
+                setContestant(data.results)
+
 
             // Catch Block
                 // Will run if the TRY section fails to run;
             } catch (error){
+                console.log(error)
 
             }
 
