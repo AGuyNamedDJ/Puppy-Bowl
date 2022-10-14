73c258a1e8aa016a652e5db9b37409a3ff19502d
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-14 15:12:04 -0500

    setting up state

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+
+// Writing Starter Code
+const puppyBowlRosterArr = ["Contestant", "Kenny"]
+
+// Declaring State
+const Contestants = () => {
+    // This is an empty starter plate;
+    const [contestant, setContestant] = useState([])
+}
