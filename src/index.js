1c2ce3d569839e195663966098e4143c83441f1d
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-14 15:27:14 -0500

    adding try & catch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 
 // Writing Starter Code
@@ -10,5 +10,27 @@ const Contestants = () => {
     const [contestant, setContestant] = useState([])
 
     // Fetch Data
-    
+    useEffect(() = > {
+        // Here we are grabbing data from the Puppl Bowl API
+        async function fetchPuppyBowlData () {
+
+            // Try Block
+                // Will TRY to run the code;
+            try {
+                // Fetch method that will get some data from the URL;
+                const response = await fetch ("https://fsa-puppy-bowl.herokuapp.com");
+                // Convert promise response into JSON;
+
+                // Sava data from response promise & update state;
+
+            // Catch Block
+                // Will run if the TRY section fails to run;
+            } catch (error){
+
+            }
+
+        };
+
+    })
+
 }
