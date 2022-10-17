491f57f4d09a1a69a2fbb9801103279b073d944b
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-17 15:32:21 -0500

    Updated index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import ReactDOM from 'react-dom';
 // Import CreateBrowserRouter Method from Dependency
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// Imported our Pages
+import AllPups from "./components/AllPups";
+import AboutUs from "./components/AboutUs";
+import ErrorPage from "./components/ErrorPage";
+import Homepage from "./components/Homepage";
+import Settings from "./components/Settings";
+
 // Now we need to use the brower in the index.js entry file to make a router instance
     // One argument needed
         // [] of objects; every object is an individual route of our react application
-const router = createBrowserRouter((
+const router = createBrowserRouter([
     {
         path: "/",
         element: < Homepage />,
@@ -15,20 +22,20 @@ const router = createBrowserRouter((
         children: [
             // mimicks the parent
             {
-                path: "/settings",
-                element: <Settings />
-            },
-            {
                 path: "/about",
-                element: <About />
+                element: <AboutUs />
             },
             {
                 path: "/allpups",
                 element: <AllPups />
             },
+            {
+                path: "/settings",
+                element: <Settings />
+            }
         ]
-    }
-))
+    }]
+);
 
 // Now you have to connect the router to the DOM
 ReactDOM.render(<RouterProvider router={router} />, document.getElementById("app"))
