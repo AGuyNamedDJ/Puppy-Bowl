ede68873577b1c55b8f41348b2b513877d7b590c
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-17 15:05:15 -0500

    Completed error page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,12 @@
+// This will be the parent Error page.
+import React from "react"; 
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <p>404 Page Not Found - Our pups got loose, be right back!</p>
+        </div>
+    )
+};
+
+export default ErrorPage; 
