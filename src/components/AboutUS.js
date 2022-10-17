199b0410a4afbc2413d1d348400176957bff84a6
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-17 14:58:24 -0500

    AboutUs page finished

---
+++ src/components/AboutUS.js
@@ -0,0 +1,14 @@
+// Initial Import;
+import React from "react";
+
+// Create the Page;
+const AboutUs = () => {
+    return (
+        <div>
+            <p>More Updates Coming Soon! </p>
+        </div>
+    )
+};
+
+// Make it useable;
+export default AboutUs; 
