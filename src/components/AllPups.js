fe4beaeb94d721b6bc2c9d6ce9d29eb59441f81a
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-17 15:03:30 -0500

    Updating allpups page complete

---
+++ src/components/AllPups.js
@@ -0,0 +1,24 @@
+// Initial Import;
+import React from "react";
+import { useOutletContext } from "react-router-dom"; 
+    // This allows us to use an outlet.
+
+const AllPups = () => {
+    const OutletContext = useOutletContext(); 
+    console.log("This is our outlet context: ", OutletContext); 
+
+    return (
+        <div>
+            <p>Check out our Pup Contestants! </p>
+            {
+                OutletContext[0].map((puppy, idx) => {
+                    return <div key={idx}>
+                        <p>Name of puppy: {puppy}</p>
+                    </div>
+                })
+            }
+        </div>
+    )
+};
+
+export default AllPups; 
