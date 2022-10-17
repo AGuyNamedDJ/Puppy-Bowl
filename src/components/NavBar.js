a803f1f11b3199a1d7d571e1a2b6de7043d6fe5f
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-10-17 12:28:07 -0500

    updating with Elles lecture

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// How to create a Navbar
+// 1) Get Link from React-Router-Dom
+import { Link, link } from "React-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+
+            <Link to="About"></Link>
+            <Link to="See All Pups"></Link>
+            <Link to="Settings"></Link>
+
+            <a href="">About Us </a>
+            <a href="">See All Pups </a>
+            <a href="">Settings </a>
+        </nav>
+    )
+};
+
+export default Navbar;
